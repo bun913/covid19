5b6dc6aa9c0381be1f8f6ad2193d3b6c4062066b
sugasaki <sugasaki@gmail.com>
2021-05-20 00:37:02 +0900

    #339 カウント数が２０００で止まってい他ので３０００に増やした

--- services/bodikApi.js
+++ services/bodikApi.js
@@ -12,7 +12,7 @@ const nagasakiCityNewsId = 'eb0ba50a-7a97-4029-9b75-9c6bab0568f6'
 const nagasakiOtherInfoId = '438f03f1-0ee8-466d-a5d9-e874f5367507'
 
 // Bodikからのデータ取得上限
-const limit = 2000
+const limit = 3000
 
 export const fetchNagasakiPrefectureTestedCases = () =>
   callFetchJsonp(baseUrl, nagasakiPrefectureTestedCasesId, { limit })
