863e823435352a29ceb5ee0b2ecd5e5eb21780fc
sugasaki <sugasaki@gmail.com>
2021-08-25 23:28:00 +0900

    #352 常に最新のデータを動的ロードを廃止

--- pages/index.vue
+++ pages/index.vue
@@ -103,7 +103,7 @@ export default Vue.extend({
   async mounted() {
     // 動的に最新情報を取得する
     await sleep(50)
-    await this.$store.dispatch('GET_BODIK_JSONP')
+    // await this.$store.dispatch('GET_BODIK_JSONP')
   },
   head(): MetaInfo {
     return {
