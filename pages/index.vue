eb23a90d5c55f9c38e2abb56f94dd0aec745f854
sugasaki <sugasaki@gmail.com>
2021-09-03 01:17:24 +0900

    再読込メッセージを追加

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,9 @@
         <span :class="[$style.alert]"
           >※ データの更新は平日（県庁稼働日）のお昼頃になります。</span
         >
+        <span :class="[$style.alert]"
+          >※ 古いデータが表示される場合には画面を再読込してください。</span
+        >
       </div>
 
       <div
